b11f6b2e31ce34623b7e73ec0ea5b41164c8876e
jleine02 <jacob.leiner@gmail.com>
2021-01-27 09:07:09 -0800

    running_time.py structure

--- my_sum.py
+++ my_sum.py
@@ -3,7 +3,7 @@
 
 def main():
     """Runs program."""
-    print(f"mysum([10, 20, 30, 40, 50], 50): {mysum([10, 20, 30, 40, 50], 50)}")
+    print(f"mysum(10, 20, 30, 40, 50, 50): {mysum([10, 20, 30, 40, 50], 50)}")
 
 def mysum(*numbers, start=0):
     sum = start
---
+++ running_time.py
@@ -0,0 +1,14 @@
+#! usr/bin/env python3
+"""Program calculates average 10k run time based off user input."""
+
+def main():
+    """Runs program."""
+    run_timing()
+
+def run_timing():
+    """Calculates average 10km run time based off user input."""
+    
+
+
+if __name__ == '__main__':
+    main()
