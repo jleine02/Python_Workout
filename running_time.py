05a670228dcec866c08a83401f15a4ca99006444
jleine02 <jacob.leiner@gmail.com>
2021-01-27 09:32:05 -0800

    created average time variable

--- running_time.py
+++ running_time.py
@@ -7,19 +7,21 @@ def main():
 
 def run_timing():
     """Calculates average 10km run time based off user input."""
+
     number_of_runs = 0
-    total_run_time = 0.0
+    total_run_time = 0
 
     while True:
         current_run_time = input("Enter 10km run time: ")
         if current_run_time:
             number_of_runs += 1
-            total_run_time += int(current_run_time)
+            total_run_time += float(current_run_time)
         else:
             break
 
+    average_time = total_run_time / number_of_runs
 
-    print(f"Average of {total_run_time / number_of_runs}, over {number_of_runs} runs")
+    print(f"Average of {average_time}, over {number_of_runs} runs")
 
 
 if __name__ == '__main__':
