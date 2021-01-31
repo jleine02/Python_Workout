5d84e390e35b1d05ff240e43bc89e38e5849372e
jleine02 <jacob.leiner@gmail.com>
2021-01-31 10:26:39 -0800

    Created structure of firstlast.

---
+++ firstlast.py
@@ -0,0 +1,17 @@
+#! usr/bin.env python3
+"""Retrieves the first and last element of the sequence."""
+
+from sys import argv
+
+def main():
+    """Runs program from command line."""
+    elements = firstlast(argv[1])
+    print(elements)
+
+def firstlast(sequence):
+    """Identifies type of sequence and returns first and last elements of that
+    sequence in the same type of sequence."""
+
+
+if __name__ == '__main__':
+    main()
