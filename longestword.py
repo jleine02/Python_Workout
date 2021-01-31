308d711ca7942517b52c1cd9abcbe30bd0d29d17
jleine02 <jacob.leiner@gmail.com>
2021-01-31 11:29:22 -0800

    Not finished, receiving errors when running.

--- longestword.py
+++ longestword.py
@@ -10,7 +10,10 @@ def main():
 
 def longestword(file):
     """Iterates over file object and returns longest string in all lines."""
-
+    with open(file, 'r') as in_file:
+        words = in_file.read().split() # opportunity to use a generator here?
+    max_len = len(max(words, key=len))
+    return [word for word in words if len(word) == max_len]
 
 if __name__ == '__main__':
     main()
