756ab4fcdfcfe009a21676a8306201aab5c9c650
jleine02 <jacob.leiner@gmail.com>
2021-02-01 07:34:07 -0800

    Finished my_sum_v2.py

--- my_sum_v2.py
+++ my_sum_v2.py
@@ -1,15 +1,18 @@
 #! usr/bin/env python3
 """Refactoring my_sum to sum an unlimitted number of elements."""
 
-from sys import argv
-
 def main():
         """Run program from command line."""
-        elements = argv[1]
-        print(my_sum(elements))
+        print(my_sum([1,2,3,4,5],[6,7,8,9,10]))
 
 def my_sum(*elements):
     """Sum a variable amount of elements regardless of type."""
+    if elements is False:
+        return elements
+    output = elements[0]
+    for element in elements[1:]:
+        output += element
+    return output
 
 
 if __name__ == '__main__':
