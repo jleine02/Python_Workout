f8a17d5e8fbb2224fec2973c87976e5bc68d0345
jleine02 <jacob.leiner@gmail.com>
2021-02-02 10:36:24 -0800

    fleshed out program.

--- sort_by_values.py
+++ sort_by_values.py
@@ -1,4 +1,13 @@
 #!/usr/bin/env python3
+"""Sort numbers by absolute value."""
+
+def main():
+    """Run from command line."""
+    print(sort_abs_value([-20, 1, 0, 37, -252, 1001]))
 
 def sort_abs_value(numbers):
+    """Sort numbers using the absolute value as a key."""
     return sorted(numbers, key=abs)
+
+if __name__ == '__main__':
+    main()
