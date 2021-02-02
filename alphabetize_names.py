61be9374b4279aee240ef6b384cb1eb7a59e6e77
jleine02 <jacob.leiner@gmail.com>
2021-02-02 10:21:55 -0800

    Used itemgetter from operator to retrieve keys for the sorted function from a list of dictionaries.

---
+++ alphabetize_names.py
@@ -0,0 +1,23 @@
+#! usr/bin/env python3
+"""Sort a dictionary data structure by key value alphabetically."""
+
+import operator
+
+def main():
+    """Run program from command line."""
+
+    PEOPLE = [{'first': 'Reuven', 'last': 'Lerner',
+               'email': 'reuven@lerner.co.il'},
+              {'first': 'Donald', 'last': 'Trump',
+               'email': 'president@whitehouse.gov'},
+              {'first': 'Vladimir', 'last': 'Putin',
+               'email': 'president@kremvax.ru'},]
+
+    print(alphabetize_names(PEOPLE))
+
+def alphabetize_names(list_of_dicts):
+    return sorted(list_of_dicts,
+        key=operator.itemgetter('last', 'first'))
+
+if __name__ == '__main__':
+    main()
