d32fd1d3d20735f01ba7c025e2f12af3c9e22a47
jleine02 <jacob.leiner@gmail.com>
2021-02-03 10:33:57 -0800

    Finished program.

--- most_repeating_word.py
+++ most_repeating_word.py
@@ -6,13 +6,18 @@ import operator
 
 def main():
     """Run program from command line."""
+    WORDS = ['potato', 'leprechaun', 'elementary', 'soup', 'alpha', 'ninny',
+            'sassafrass']
+    print(most_repeating_word(WORDS))
 
 def most_repeating_word(list_of_words):
-    """Placeholder."""
+    """Uses most_repeating_letter_count function to identify word in list with
+    the greatest number of repeating characters."""
+    return max(list_of_words, key=most_repeating_letter_count)
 
 def most_repeating_letter_count(word):
-    """Placeholder."""
-
+    """Returns the count of the letter that appears the most in the word."""
+    return Counter(word).most_common(1)[0][1]
 
 if __name__ == '__main__':
     main()
