6c41054e5b0f57b1471f2e6a41222c418f64de9c
jleine02 <jacob.leiner@gmail.com>
2021-02-03 10:12:17 -0800

    Basic structure of program added.

---
+++ most_repeating_word.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""Identifies and returns word in a list with the most repeating letters."""
+
+from collections import Counter
+
+def main():
+    """Run program from command line."""
+
+def most_repeating_word(list_of_words):
+    """Placeholder."""
+
+
+if __name__ == '__main__':
+    main()
