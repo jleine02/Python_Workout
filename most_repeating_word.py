f1a990a6fb5c35f1a7e39b2f4ab8b7ac637913db
jleine02 <jacob.leiner@gmail.com>
2021-02-03 10:24:43 -0800

    Added another helper function.

--- most_repeating_word.py
+++ most_repeating_word.py
@@ -2,11 +2,15 @@
 """Identifies and returns word in a list with the most repeating letters."""
 
 from collections import Counter
+import operator
 
 def main():
     """Run program from command line."""
 
 def most_repeating_word(list_of_words):
+    """Placeholder."""
+
+def most_repeating_letter_count(word):
     """Placeholder."""
 
 
