449743019c3ee5d0b434f1ed934d81eeb29bc2c4
jleine02 <jacob.leiner@gmail.com>
2021-02-08 14:30:11 -0800

    start of format_sort_records

---
+++ format_sort_records.py
@@ -0,0 +1,20 @@
+#! usr/bin/env python3
+"""Read and print a list of tuples in a formatted manner."""
+
+import operator
+
+def main():
+    """Run program from command line."""
+    PEOPLE = [('Donald', 'Trump', 7.85),
+              ('Valdimir', 'Putin', 3.626),
+              ('Jinping', 'Xi', 10.603)]
+
+    print(format_sort_records(PEOPLE))
+
+def format_sort_records(record):
+    """Place holder."""
+        output = []
+        template =
+
+if __name__ = '__main__':
+    main()
