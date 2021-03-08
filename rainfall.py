8293fc06ec3a69f380c1c0f12b8abad395fe913e
jleine02 <jacob.leiner@gmail.com>
2021-03-08 09:12:14 -0800

    initial structure of rainfall program

---
+++ rainfall.py
@@ -0,0 +1,12 @@
+#! usr/bin/env python3
+""" rainfall.py - program uses dictionary to keep track of user supplied
+rainfall amounts for different cities."""
+
+from collections import OrderedDict
+
+def main():
+    get_rainfall()
+
+
+def get_rainfall():
+    rainfall = {}
