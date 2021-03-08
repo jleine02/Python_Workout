ce7889020131eaf33a257c4c8fea701ddf493eaf
jleine02 <jacob.leiner@gmail.com>
2021-03-08 09:23:46 -0800

    finished rainfall.py

--- rainfall.py
+++ rainfall.py
@@ -4,9 +4,25 @@ rainfall amounts for different cities."""
 
 from collections import OrderedDict
 
+
 def main():
     get_rainfall()
 
 
 def get_rainfall():
-    rainfall = {}
+    rain_fall = OrderedDict()
+
+    while True:
+        city_name = input("Enter city name: ")
+        if city_name:
+            rain_amount = input("Enter rain amount in mm: ")
+            rain_fall[city_name] = rain_fall.get(city_name, 0) + int(rain_amount)
+        else:
+            break
+
+    for city_name, rain_amount in rain_fall.items():
+        print(f'{city_name}: {rain_amount}')
+
+
+if __name__ == "__main__":
+    main()
