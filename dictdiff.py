30072d4ff2ebf3328eb2fdec3b31ab6a91a0bf6a
jleine02 <jacob.leiner@gmail.com>
2021-03-08 11:06:05 -0800

    Fixed typo in main.

--- dictdiff.py
+++ dictdiff.py
@@ -5,14 +5,14 @@
 def main():
     d1 = {'a': 1, 'b': 2, 'c': 3}
     d2 = {'a': 1, 'b': 2, 'c': 4}
-    print(f'''difference between {d1} and {d2} is:\n{dictdiff(d1, d2)}\n''')
+    print(f'''The difference between {d1} and {d2} is:\n{dictdiff(d1, d2)}\n''')
 
     d3 = {'a': 1, 'b': 2, 'd': 3}
     d4 = {'a': 1, 'b': 2, 'c': 4}
-    print(f'''difference between {d3} and {d4} is:\n{dictdiff(d3, d4)}\n''')
+    print(f'''The difference between {d3} and {d4} is:\n{dictdiff(d3, d4)}\n''')
 
     d5 = {'a': 1, 'b': 2, 'd': 4}
-    print(f'''difference between {d1} and {d5} is\n{dictdiff(d1, d5)}''')
+    print(f'''The difference between {d1} and {d5} is:\n{dictdiff(d1, d5)}''')
 
 
 def dictdiff(dict_one, dict_two):
