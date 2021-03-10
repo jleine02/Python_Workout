15a6ffb16aa64d74c0e836d628c9abbc9f231b80
jleine02 <jacob.leiner@gmail.com>
2021-03-10 06:53:14 -0800

    initial structure of program.

---
+++ get_final_line.py
@@ -0,0 +1,14 @@
+#! usr/bin/env python3
+# get_final_line - Returns the final line in a file.
+
+
+def main():
+    get_final_line(file)
+
+
+def get_final_line(file):
+
+
+
+if __name__ == '__main__':
+    main()
