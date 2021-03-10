003475f337f1fc8844f7bc15628b049047da7b35
jleine02 <jacob.leiner@gmail.com>
2021-03-10 07:33:53 -0800

    finished program

--- get_final_line.py
+++ get_final_line.py
@@ -1,13 +1,22 @@
 #! usr/bin/env python3
 # get_final_line - Returns the final line in a file.
 
+from sys import argv
+
 
 def main():
-    get_final_line(file)
+    file_to_open = argv[1]
+    final_line = get_final_line(file_to_open)
+    print(f'The final line of the file is:\n\n{final_line}')
 
 
 def get_final_line(file):
-
+    """Returns the final line of the file passed in as a parameter."""
+    with open(file, 'r') as f:
+        final_line = ''
+        for current_line in f:
+            final_line = current_line
+    return final_line
 
 
 if __name__ == '__main__':
