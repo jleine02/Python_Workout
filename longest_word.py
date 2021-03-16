36c983e1c63ceacf4d7be29fab1b57ad3045706c
Jake Leiner <jacob.leiner@gmail.com>
2021-03-16 09:07:18 -0700

    initial commit with basic structure sans logic

---
+++ longest_word.py
@@ -0,0 +1,19 @@
+#! usr/bin/env python3
+"""longest_word.py - reads files and returns information on the longest words included in each."""
+
+from sys import argv
+
+
+def main():
+
+
+def find_longest_word(file):
+    """Returns the longest word found within the file."""
+
+
+def find_all_longest_words(*files):
+    """Return a dictionary that uses the filename as key and the longest word as the value."""
+
+
+if __name__ == '__main__':
+    main()
