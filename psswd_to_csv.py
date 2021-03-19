376a4b356766ecdb5112fb3ae8ffff1fb8a41b8c
Jake Leiner <jacob.leiner@gmail.com>
2021-03-19 11:47:25 -0700

    created logic for main method

--- psswd_to_csv.py
+++ psswd_to_csv.py
@@ -7,9 +7,14 @@ import csv
 def main():
 
 
-def passwd_to_csv(infile, outfile):
-    """"""
-
+def passwd_to_csv(infile_name, outfile_name):
+    """Writes user_name and user_id from infile to a csv using tabs as delimiter."""
+    with open(infile_name, 'r') as passwd, open(outfile_name, 'w') as output:
+        infile = csv.reader(passwd, delimiter=':')
+        outfile = csv.writer(output, delimiter='\t')
+        for record in infile:
+            if len(record) > 1:
+                outfile.writerow((record[0], record[2]))
 
 if __name__ == '__main__':
     main()
