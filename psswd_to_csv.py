9496f0bb39bb680d947f60305d07fdd3400cc0f5
Jake Leiner <jacob.leiner@gmail.com>
2021-03-19 11:36:31 -0700

    basic file structure created

---
+++ psswd_to_csv.py
@@ -0,0 +1,15 @@
+#! usr/bin/env python3
+"""psswd_to_csv.py - takes passwd-style file as input and creates csv output of username and user id."""
+
+import csv
+
+
+def main():
+
+
+def passwd_to_csv(infile, outfile):
+    """"""
+
+
+if __name__ == '__main__':
+    main()
